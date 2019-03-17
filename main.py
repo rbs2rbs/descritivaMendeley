64dea0cc0df5d3fa0e7df8d3baf3ea6f06189ca8
rbs2rbs <renan.bisposilva@gmail.com>
2019-03-17 00:45:51 -0300

    Adciona anlise das palavras chaves

--- main.py
+++ main.py
@@ -1,77 +1,11 @@
 from scholar import *
-import matplotlib.pyplot as plt
-# plt.style.use('seaborn-whitegrid')
-# Create random data with numpy
-import numpy as np
-import pandas as pd
-
-import collections
-
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize 
-
 
 dados = Scholar(
     busca = 'machine learning', 
     tipo = 'abstract', 
-    token = 'MSwxNTUyNzY4MzcxODI1LDU0MDU5NTI5MSwxMDI4LGFsbCwsLDIxM2YzYWNhNDIwNjkzNGNhYzU4ZDdlLTFkZWZmN2I4MmY3Zmd4cnFiLDQ1MGE3NzZiLWRhMjEtMzk5MC05NTZiLTY3ZTZkZWMzM2E1NyxOZVVGVkl3MnRHMjFZMzFuX3R2M1VhcmhKajg'
+    token = 'MSwxNTUyNzkzOTk4Njg5LDU0MDU5NTI5MSwxMDI4LGFsbCwsLGVlYjZkMTlkODNkNzYyNGM1ODM4NjYwLTQ1YmEzYTc3YWU2Mmd4cnFiLDQ1MGE3NzZiLWRhMjEtMzk5MC05NTZiLTY3ZTZkZWMzM2E1NyxiYW03OFNNU0dvXzdaaG5lWnpoMERjeFRxZXc'
 )
 
 dados.gerar()
-# ANALISE ANAO
-# anos = []
-# for ano in dados.saida:
-#     anos.append(ano['ano'])
 
-# anos_np = np.unique(anos, return_counts=True)
-# print(anos_np)
-# for index, i in enumerate(range(min(anos_np[0]),max(anos_np[0])+1)):
-#     if i not in anos:
-#         anos_np = np.insert(anos_np,[index],[[i],[0]], axis=1)
-
-# plt.plot(anos_np[0], anos_np[1], 'r--')
-# plt.show()
-
-# ANALISE TITULO
-# stop_words_in = set(stopwords.words('english')) 
-# stop_words_pt = set(stopwords.words('portuguese')) 
-
-# titulo_filtrado = [] 
-# for titulo in dados.saida:
-#     word_tokens = word_tokenize(titulo['titulo']) 
-#     for w in word_tokens:
-#         if not (w.lower() in stop_words_in or w.lower() in stop_words_pt):
-#             if w.isalnum():
-#                 titulo_filtrado.append(w.lower())
-
-# titulo_filtrado = collections.Counter(titulo_filtrado).most_common()
-
-# df = pd.DataFrame(titulo_filtrado[:10])
-
-# p = df.plot(kind='barh', title ="Palavras Frequentes", figsize=(15, 10), legend=False, fontsize=12)
-# p.set_yticklabels(df[0])
-# p.invert_yaxis()
-# plt.show()
-
-
-# ANALISE RESUMO
-
-stop_words_in = set(stopwords.words('english')) 
-stop_words_pt = set(stopwords.words('portuguese')) 
-
-titulo_filtrado = [] 
-for titulo in dados.saida:
-    word_tokens = word_tokenize(titulo['resumo']) 
-    for w in word_tokens:
-        if not (w.lower() in stop_words_in or w.lower() in stop_words_pt):
-            if w.isalnum():
-                titulo_filtrado.append(w.lower())
-
-titulo_filtrado = collections.Counter(titulo_filtrado).most_common()
-
-df = pd.DataFrame(titulo_filtrado[:10])
-
-p = df.plot(kind='barh', title ="Palavras Frequentes", figsize=(15, 10), legend=False, fontsize=12)
-p.set_yticklabels(df[0])
-p.invert_yaxis()
-plt.show()
+grafico(t = "palavras-chave", dados = dados)
--- scholar.py
+++ scholar.py
@@ -5,6 +5,16 @@ import requests
 # UTILIZAR REGEX PARA ESTRAIR TEXTOS
 import re
 
+import matplotlib.pyplot as plt
+
+# Create random data with numpy
+import numpy as np
+import pandas as pd
+
+import collections
+
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
 
 
 class Scholar(object):
@@ -23,17 +33,27 @@ class Scholar(object):
   def gerar(self):
     print(self.req.status_code)
     if self.req.status_code != 200:
-      print('Token Expirado')
+      print('Token Expirado\nhttps://mendeley-show-me-access-tokens.herokuapp.com/')
       return('Token Expirado')
     else:
       for dados in self.req.json():
-        self.saida.append(
-          {
-            'titulo' : dados['title'],
-            'ano' : dados['year'],
-            'resumo' : dados['abstract']
-          }
-        )
+        if "keywords" in dados.keys():
+          self.saida.append(
+            {
+              'titulo' : dados['title'],
+              'ano' : dados['year'],
+              'resumo' : dados['abstract'],
+              'palavras' : dados['keywords']
+            }
+          )
+        else :
+          self.saida.append(
+            {
+              'titulo' : dados['title'],
+              'ano' : dados['year'],
+              'resumo' : dados['abstract']
+            }
+          )
 
       proximo = self.req.links
       if proximo:
@@ -42,66 +62,106 @@ class Scholar(object):
           req = requests.get(url, headers = self.headers, timeout = 5)
           print(req.status_code)
           for dados in req.json():
-            self.saida.append(
-              {
-                'titulo' : dados['title'],
-                'ano' : dados['year'],
-                'resumo' : dados['abstract']
-              }
-            )
+            if "keywords" in dados.keys():
+              self.saida.append(
+                {
+                  'titulo' : dados['title'],
+                  'ano' : dados['year'],
+                  'resumo' : dados['abstract'],
+                  'palavras' : dados['keywords']
+                }
+              )
+            else :
+              self.saida.append(
+                {
+                  'titulo' : dados['title'],
+                  'ano' : dados['year'],
+                  'resumo' : dados['abstract']
+                }
+              )
+                
           proximo = req.links    
 
     self.qt_artigos = len(self.saida)/2
-    # self.titulos = self.saida['titulo']
-    # self.anos = self.saida['ano']
+   
 
+def grafico(t,dados):
+  if not t in ["ano","titulo","resumo","palavras-chave"]:
+      return ("Tipo de grafico Inválido")
+  if t == "ano":
+    # ANALISE ANO
 
+    anos = []
+    for ano in dados.saida:
+        anos.append(ano['ano'])
 
+    anos_np = np.unique(anos, return_counts=True)
+    for index, i in enumerate(range(min(anos_np[0]),max(anos_np[0])+1)):
+        if i not in anos:
+            anos_np = np.insert(anos_np,[index],[[i],[0]], axis=1)
 
+    plt.plot(anos_np[0], anos_np[1], 'r--')
+    plt.show()
 
+  elif t == "titulo":
+    # ANALISE TITULO
 
+    stop_words_in = set(stopwords.words('english')) 
+    stop_words_pt = set(stopwords.words('portuguese')) 
 
+    titulo_filtrado = [] 
+    for titulo in dados.saida:
+        word_tokens = word_tokenize(titulo['titulo']) 
+        for w in word_tokens:
+            if not (w.lower() in stop_words_in or w.lower() in stop_words_pt):
+                if w.isalnum():
+                    titulo_filtrado.append(w.lower())
 
-# url = 'https://api.mendeley.com/search/catalog?title=%22machine%20learning%22&limit=100'      
-# headers = {
-#   'Authorization': 'Bearer MSwxNTUyMzQ3NTA1MTUzLDU0MDU5NTI5MSwxMDI4LGFsbCwsLDFlZTIwZTMzNzkzYmY3NDkzYjdhNjg1NzYyNmRhOTg1MGYzNGd4cnFiLDQ1MGE3NzZiLWRhMjEtMzk5MC05NTZiLTY3ZTZkZWMzM2E1NyxGMWZNSWg2MXZWTnlMTHRSLU5USzFlM1hLZnc',
-#   'Accept' : 'application/vnd.mendeley-document.1+json'
-#   }
+    titulo_filtrado = collections.Counter(titulo_filtrado).most_common()
 
-# req = requests.get(url, headers = headers, timeout = 5)
-# saida = []
-# if req.status_code != 200:
-#   print('Token Expirado')
-# else:
-#   for dados in req.json():
-#     saida.append(
-#       {
-#         'titulo' : dados['title'],
-#         'ano' : dados['year']
-#       }
-#     )
+    df = pd.DataFrame(titulo_filtrado[:10])
 
-#   proximo = req.links
-#   if proximo:
-#     while proximo:
-#       url = proximo['next']['url']   
-#       print(url)  
-#       headers = {
-#       'Authorization': 'Bearer MSwxNTUyMzQ3NTA1MTUzLDU0MDU5NTI5MSwxMDI4LGFsbCwsLDFlZTIwZTMzNzkzYmY3NDkzYjdhNjg1NzYyNmRhOTg1MGYzNGd4cnFiLDQ1MGE3NzZiLWRhMjEtMzk5MC05NTZiLTY3ZTZkZWMzM2E1NyxGMWZNSWg2MXZWTnlMTHRSLU5USzFlM1hLZnc',
-#       'Accept' : 'application/vnd.mendeley-document.1+json'
-#       }
+    p = df.plot(kind='barh', title ="Palavras Frequentes", figsize=(15, 10), legend=False, fontsize=12)
+    p.set_yticklabels(df[0])
+    p.invert_yaxis()
+    plt.show()
 
-#       req = requests.get(url, headers = headers, timeout = 5)
-#       for dados in req.json():
-#         saida.append(
-#           {
-#             'titulo' : dados['title'],
-#             'ano' : dados['year']
-#           }
-#         )
-#       proximo = req.links
-#       print(proximo)
+  elif t == "resumo":
 
+    # ANALISE RESUMO
 
+    stop_words_in = set(stopwords.words('english')) 
+    stop_words_pt = set(stopwords.words('portuguese')) 
 
-# print(saida)
+    resumo_filtrado = [] 
+    for resumo in dados.saida:
+        word_tokens = word_tokenize(titulo['resumo']) 
+        for w in word_tokens:
+            if not (w.lower() in stop_words_in or w.lower() in stop_words_pt):
+                if w.isalnum():
+                    resumo_filtrado.append(w.lower())
+
+    resumo_filtrado = collections.Counter(resumo_filtrado).most_common()
+
+    df = pd.DataFrame(resumo_filtrado[:10])
+
+    p = df.plot(kind='barh', title ="Palavras Frequentes", figsize=(15, 10), legend=False, fontsize=12)
+    p.set_yticklabels(df[0])
+    p.invert_yaxis()
+    plt.show()
+  
+  elif t == "palavras-chave":
+    palavra_filtrado = []
+
+    for palavra in dados.saida:
+      if "palavras" in palavra.keys():
+        palavra_filtrado.append(palavra['palavras'])
+
+    palavra_filtrado = collections.Counter(palavra_filtrado).most_common()
+
+    df = pd.DataFrame(palavra_filtrado[:10])
+
+    p = df.plot(kind='barh', title ="Palavras Frequentes", figsize=(15, 10), legend=False, fontsize=12)
+    p.set_yticklabels(df[0])
+    p.invert_yaxis()
+    plt.show()
