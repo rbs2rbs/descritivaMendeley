86068368e8f12fdf5218cc16f8a025a94c4b2f82
rbs2rbs <renan.bisposilva@gmail.com>
2019-04-07 21:16:35 -0300

    Atualiza erros de ano para Lonardo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scholar import *
 
 dados = Scholar(
-    busca = 'machine learning', 
+    busca = 'Pau da Lima', 
     tipo = 'abstract', 
-    token = 'MSwxNTUyNzkzOTk4Njg5LDU0MDU5NTI5MSwxMDI4LGFsbCwsLGVlYjZkMTlkODNkNzYyNGM1ODM4NjYwLTQ1YmEzYTc3YWU2Mmd4cnFiLDQ1MGE3NzZiLWRhMjEtMzk5MC05NTZiLTY3ZTZkZWMzM2E1NyxiYW03OFNNU0dvXzdaaG5lWnpoMERjeFRxZXc'
+    token = 'MSwxNTU0Njg1NzExMzQ2LDU0MDU5NTI5MSwxMDI4LGFsbCwsLGI5NGM3MmRjNDM5M2Q2NDYwZDZiM2U0NDVlYWRkY2IyM2Q1OWd4cnFiLDQ1MGE3NzZiLWRhMjEtMzk5MC05NTZiLTY3ZTZkZWMzM2E1NyxsSkdHbFJxTTc5UlpRTVQyTjJsZ0syMmY0WEU'
 )
 
 dados.gerar()
